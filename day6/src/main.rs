2ce29f737a0add86623726c3cad06a85d8e15fbd
The0x539 <the0x539@gmail.com>
2021-12-05 23:21:08 -0600

    day 6: simpler state

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,39 +1,32 @@
-use std::collections::BTreeMap;
-
-type State = BTreeMap<u8, u64>;
-
-fn make_state() -> State {
-    (0..=8).zip([0; 9]).collect()
-}
+type State = [u64; 9];
 
 fn parse_input(s: &str) -> State {
-    let mut state = make_state();
-    for n in s.split(",").map(|t| t.parse().unwrap()) {
-        *state.entry(n).or_default() += 1;
+    let mut state = [0; 9];
+    for n in s.split(",").map(|t| t.parse::<usize>().unwrap()) {
+        state[n] += 1;
     }
     state
 }
 
 fn run(mut state: State, time: u16) -> u64 {
     for _ in 0..time {
-        let mut new_state = make_state();
+        let mut new_state = [0; 9];
         for i in 1..=8 {
-            new_state.insert(i - 1, state[&i]);
+            new_state[i - 1] = state[i];
         }
-        let n_zero = state[&0];
-        *new_state.get_mut(&8).unwrap() += n_zero;
-        *new_state.get_mut(&6).unwrap() += n_zero;
+        new_state[6] += state[0];
+        new_state[8] += state[0];
         state = new_state;
     }
-    state.values().sum()
+    state.into_iter().sum()
 }
 
 fn part1(input: &[State]) -> u64 {
-    run(input[0].clone(), 80)
+    run(input[0], 80)
 }
 
 fn part2(input: &[State]) -> u64 {
-    run(input[0].clone(), 256)
+    run(input[0], 256)
 }
 
 util::register!(parse_input, part1, part2);
