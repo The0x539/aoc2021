5d75c5c61ed3c9be6b762cd31ed94d8210e666bb
The0x539 <the0x539@gmail.com>
2021-12-11 23:53:13 -0600

    day 12: cleaner solution

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,7 +1,6 @@
 use std::collections::{HashMap, HashSet};
 
 struct Input(String, String);
-
 type Output = usize;
 
 fn is_lowercase(s: &str) -> bool {
@@ -13,7 +12,29 @@ fn parse(s: &str) -> Input {
     Input(a.to_owned(), b.to_owned())
 }
 
-fn part1(input: &[Input]) -> Output {
+#[derive(Clone)]
+struct Path {
+    location: String,
+    history: HashSet<String>,
+    can_repeat: bool,
+}
+
+impl Path {
+    fn new(can_repeat: bool) -> Self {
+        Self {
+            location: "start".to_owned(),
+            history: std::iter::once("start".to_owned()).collect(),
+            can_repeat,
+        }
+    }
+
+    fn visit(&mut self, dst: &str) {
+        self.location = dst.to_owned();
+        self.history.insert(dst.to_owned());
+    }
+}
+
+fn run(input: &[Input], can_repeat: bool) -> Output {
     let connectivity = {
         let mut c = HashMap::<String, HashSet<String>>::new();
         for Input(a, b) in input {
@@ -23,81 +44,45 @@ fn part1(input: &[Input]) -> Output {
         c
     };
 
-    let mut paths = vec![vec!["start".to_owned()]];
+    let mut incomplete_paths = vec![Path::new(can_repeat)];
+    let mut num_complete_paths = 0;
 
-    loop {
-        let mut is_done = true;
-        let mut new_paths = vec![];
-
-        for path in paths {
-            let last = path.last().unwrap();
-            if last == "end" {
-                new_paths.push(path);
+    while let Some(path) = incomplete_paths.pop() {
+        for dst in &connectivity[&path.location] {
+            if dst == "start" {
+                continue;
+            } else if dst == "end" {
+                num_complete_paths += 1;
                 continue;
             }
-            for dst in &connectivity[last] {
-                if is_lowercase(dst) && path.contains(dst) {
+
+            let mut new_path;
+
+            if is_lowercase(dst) && path.history.contains(dst) {
+                if path.can_repeat {
+                    new_path = path.clone();
+                    new_path.can_repeat = false;
+                } else {
                     continue;
                 }
-                is_done = false;
-                let mut new_path = path.clone();
-                new_path.push(dst.clone());
-                new_paths.push(new_path);
+            } else {
+                new_path = path.clone();
             }
-        }
 
-        paths = new_paths;
-        if is_done {
-            break;
+            new_path.visit(dst);
+            incomplete_paths.push(new_path);
         }
     }
-    paths.len()
+
+    num_complete_paths
+}
+
+fn part1(input: &[Input]) -> Output {
+    run(input, false)
 }
 
 fn part2(input: &[Input]) -> Output {
-    let connectivity = {
-        let mut c = HashMap::<String, HashSet<String>>::new();
-        for Input(a, b) in input {
-            c.entry(a.clone()).or_default().insert(b.clone());
-            c.entry(b.clone()).or_default().insert(a.clone());
-        }
-        c
-    };
-
-    let mut paths = vec![(vec!["start".to_owned()], false)];
-
-    loop {
-        let mut is_done = true;
-        let mut new_paths = vec![];
-
-        for path in paths {
-            let last = path.0.last().unwrap();
-            if last == "end" {
-                new_paths.push(path);
-                continue;
-            }
-            for dst in &connectivity[last] {
-                let mut new_path = path.clone();
-                if is_lowercase(dst) && path.0.contains(dst) {
-                    if path.1 || dst == "start" {
-                        continue;
-                    } else {
-                        new_path.1 = true;
-                    }
-                }
-                is_done = false;
-                new_path.0.push(dst.clone());
-                new_paths.push(new_path);
-            }
-        }
-
-        paths = new_paths;
-        if is_done {
-            break;
-        }
-    }
-
-    paths.len()
+    run(input, true)
 }
 
 util::register!(parse, part1, part2);
