f98ca0b4f6b1a3c9f37e6d0124d16b982d7cf323
The0x539 <the0x539@gmail.com>
2021-12-14 00:13:11 -0600

    day 14

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,3 +1,5 @@
+#![feature(array_windows)]
+
 use std::collections::HashMap;
 type Rules = HashMap<(u8, u8), u8>;
 
@@ -6,7 +8,7 @@ struct Input {
     rules: Rules,
 }
 
-type Output = usize;
+type Output = u64;
 
 fn parse(s: &str) -> Input {
     let mut lines = s.lines();
@@ -22,24 +24,28 @@ fn parse(s: &str) -> Input {
     Input { template, rules }
 }
 
+#[allow(dead_code)]
 fn apply(state: Vec<u8>, rules: &Rules) -> Vec<u8> {
     let mut new_state = vec![];
     for i in 0..state.len() - 1 {
         new_state.push(state[i]);
         if let Some(insertion) = rules.get(&(state[i], state[i + 1])) {
             new_state.push(*insertion);
+        } else {
+            println!("didn't insert");
         }
     }
     new_state.push(*state.last().unwrap());
     new_state
 }
 
-fn part1(inp: &Input) -> Output {
+#[allow(dead_code)]
+fn part1_old(inp: &Input) -> Output {
     let mut state = inp.template.clone();
     for _ in 0..10 {
         state = apply(state, &inp.rules);
     }
-    let mut frequencies = HashMap::<u8, usize>::new();
+    let mut frequencies = HashMap::<u8, u64>::new();
     for elem in state {
         *frequencies.entry(elem).or_default() += 1;
     }
@@ -48,19 +54,69 @@ fn part1(inp: &Input) -> Output {
     max - min
 }
 
+#[derive(Default)]
+struct State {
+    pair_counts: HashMap<(u8, u8), u64>,
+    char_counts: HashMap<u8, u64>,
+}
+
+impl State {
+    fn new(template: &[u8]) -> Self {
+        let mut state = Self::default();
+        for &[a, b] in template.array_windows() {
+            state.insert_pair(a, b, 1);
+            state.insert_char(a, 1);
+            state.insert_char(b, 1);
+        }
+        state
+    }
+
+    fn apply(&self, rules: &Rules) -> Self {
+        let mut new_state = Self::default();
+        new_state.char_counts = self.char_counts.clone();
+        for (&(a, b), &n) in &self.pair_counts {
+            if let Some(&c) = rules.get(&(a, b)) {
+                new_state.insert_pair(a, c, n);
+                new_state.insert_pair(c, b, n);
+                new_state.insert_char(c, n);
+            } else {
+                println!("no insertion");
+                new_state.insert_pair(a, b, n);
+            }
+        }
+
+        new_state
+    }
+
+    fn insert_pair(&mut self, a: u8, b: u8, n: u64) {
+        *self.pair_counts.entry((a, b)).or_default() += n;
+    }
+
+    fn insert_char(&mut self, a: u8, n: u64) {
+        *self.char_counts.entry(a).or_default() += n;
+    }
+
+    fn result(&self) -> u64 {
+        let max = self.char_counts.values().max().unwrap();
+        let min = self.char_counts.values().min().unwrap();
+        (max - min) + 2 // ??????????
+    }
+}
+
+fn run(inp: &Input, steps: u8) -> Output {
+    let mut state = State::new(&inp.template);
+    for _ in 0..steps {
+        state = state.apply(&inp.rules);
+    }
+    state.result()
+}
+
+fn part1(inp: &Input) -> Output {
+    run(inp, 10)
+}
+
 fn part2(inp: &Input) -> Output {
-    let mut state = inp.template.clone();
-    for i in 0..40 {
-        println!("{}: {}", i, state.len());
-        state = apply(state, &inp.rules);
-    }
-    let mut frequencies = HashMap::<u8, usize>::new();
-    for elem in state {
-        *frequencies.entry(elem).or_default() += 1;
-    }
-    let min = frequencies.values().copied().min().unwrap();
-    let max = frequencies.values().copied().max().unwrap();
-    max - min
+    run(inp, 40)
 }
 
 fn main() {
@@ -73,7 +129,7 @@ fn main() {
 #[cfg(test)]
 #[test]
 fn test() {
-    let (a, b) = util::parse_output::<usize>();
+    let (a, b) = util::parse_output::<u64>();
     let input = std::fs::read_to_string("test.txt").unwrap();
     let input = parse(&input);
     assert_eq!(a, part1(&input));
